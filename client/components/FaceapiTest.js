e7fd59723985f01ffb3e50f37b2278321f5e014c
ANN LIN <smilemarg@gmail.com>
2019-08-15 13:05:31 -0400

    temp

--- client/components/FaceapiTest.js
+++ client/components/FaceapiTest.js
@@ -6,8 +6,20 @@ class FaceapiTest extends Component {
     super()
     this.state = {
       input1: null,
-      input2: null
+      input2: null,
+      distance: 1
     }
+  }
+
+  computeFaceDescriptor = async () => {
+    await faceapi.loadFaceRecognitionModel('/models')
+    console.log('1')
+    const desc1 = await faceapi.computeFaceDescriptor(this.state.input1)
+    console.log('2')
+    const desc2 = await faceapi.computeFaceDescriptor(this.state.input2)
+    console.log('3')
+    const distance = faceapi.round(faceapi.euclideanDistance(desc1, desc2))
+    this.setState({distance})
   }
 
   async componentDidMount() {
@@ -17,18 +29,13 @@ class FaceapiTest extends Component {
   }
 
   render() {
-    console.log('I am here')
-    console.log(faceapi.fetchImage)
-    console.log(faceapi.computeFaceDescriptor)
-    console.log(faceapi.euclideanDistance)
     console.log(this.state)
-    //run functions here
-    //when results ready
-    //change state for re render
 
     return (
       <div>
         <h1>Face-api Test Page</h1>
+        <button onClick={this.computeFaceDescriptor}>Compute Face</button>
+        <h2>Result: {this.state.distance}</h2>
       </div>
     )
   }
