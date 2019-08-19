79abc4bf5f589d2a110a881acad65f924f2b3e40
ANN LIN <smilemarg@gmail.com>
2019-08-19 18:03:01 -0400

    imgur not finished

--- client/components/UploadBWFace.js
+++ client/components/UploadBWFace.js
@@ -1,78 +1,6 @@
-// import React, {Component} from 'react'
-// import axios from 'axios'
-// import aws from 'aws-sdk'
-
-// class UploadBWFace extends Component {
-//   constructor() {
-//     super()
-//     this.state = {
-//       img: null
-//     }
-//   }
-
-//   handleSubmit = e => {
-//     e.preventDefault()
-//     //const img = e.target.img.files[0].name
-//     const file = e.target.img.files[0]
-
-//     //config
-//     aws.config.update({
-//       region: 'us-east-1',
-//       credentials: new aws.SharedIniFileCredentials()
-//     })
-
-//     //save img to s3
-//     var s3 = new aws.S3()
-
-//     var params = {
-//       Bucket: 'build-a-mate-img',
-//       Key: file.name,
-//       Expires: 60,
-//       ContentType: file.type
-//     }
-
-//     // POST works differently in s3, use PUT to add object to bucket
-//     s3.getSignedUrl('putObject', params, async function(err, signedUrl) {
-//       if (err) {
-//         console.log(err)
-//         return err
-//       } else {
-//         console.log('SIGNED URL:', signedUrl)
-
-//         await axios
-//           .put(signedUrl, file, {headers: {'Content-Type': file.type}})
-//           .then(function(result) {
-//             console.log('RESULT:', result)
-//           })
-//           .catch(function(err) {
-//             console.log(err)
-//           })
-//       }
-//     })
-
-//     this.setState({img: file.name})
-//   }
-
-//   render() {
-//     console.log(this.state)
-//     return (
-//       <div>
-//         <h1>Upload Face</h1>
-//         <form onSubmit={this.handleSubmit}>
-//           <input type="file" id="bw-face" name="img" accept="image/png" />
-//           <input type="submit" value="Upload Image" name="submit" />
-//         </form>
-//       </div>
-//     )
-//   }
-// }
-
-// export default UploadBWFace
-
 import React, {Component} from 'react'
 import Webcam from 'react-webcam'
-// import axios from 'axios'
-// import aws from 'aws-sdk'
+import axios from 'axios'
 require('../../secrets')
 
 class UploadBWFace extends Component {
@@ -81,23 +9,15 @@ class UploadBWFace extends Component {
     this.state = {
       file: null
     }
-    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleSubmit(e) {
+  handleSubmit = async e => {
     e.preventDefault()
     //const img = e.target.img.files[0].name
-    const config = {
-      bucketName: process.env.S3_BUCKET,
-      dirName: 'black_n_white' /* optional */,
-      region: 'us-east-1',
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
+    const file = e.target.img.files[0]
 
-    S3FileUpload.uploadFile(this.state.file, config)
-      .then(data => console.log(data))
-      .catch(err => console.error(err))
+    await axios.post('api/imgur/uploadbwface', file)
+    this.setState({file})
   }
 
   setRef = webcam => {
@@ -120,19 +40,21 @@ class UploadBWFace extends Component {
     }
     return (
       <div>
-        <Webcam
+        {/* <Webcam
           audio={false}
           height={350}
           ref={this.setRef}
           screenshotFormat="image/png"
           width={350}
           videoConstraints={videoConstraints}
-        />
+        /> */}
         <h1>Upload Face</h1>
         <form onSubmit={this.handleSubmit}>
-          <button type="submit" onClick={this.capture}>
+          {/* <button type="submit" onClick={this.capture}>
             Capture photo
           </button>
+          <input type="submit" value="Upload Image" name="submit" /> */}
+          <input type="file" id="bw-face" name="img" accept="image/png" />
           <input type="submit" value="Upload Image" name="submit" />
         </form>
       </div>
