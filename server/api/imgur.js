1bfd87b16a8bd97c50c1ac7c75df5fc986f4452a
ANN LIN <smilemarg@gmail.com>
2019-08-20 15:47:36 -0400

    imgur routes get and post

--- server/api/imgur.js
+++ server/api/imgur.js
@@ -1,37 +1,33 @@
 const router = require('express').Router()
 const axios = require('axios')
 //const Imgur = require('imgur-node')
-const imgur = require('imgur')
+// const imgur = require('imgur')
+// const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
 require('../../secrets')
 //const client = new Imgur.Client(process.env.IMGUR_CLIENT_ID)
 module.exports = router
 
 router.post('/uploadbwface', async (req, res, next) => {
   try {
-    // imgur.setClientId(process.env.IMGUR_CLIENT_ID)
-    // const id = 'qizwMyP'
-    // imgur.setAPIUrl(`https://api.imgur.com/3/album/${id}/images`)
-
-    // imgur.setCredentials(process.env.IMGUR_EMAIL, process.env.IMGUR_PASSWORD, process.env.IMGUR_CLIENT_ID)
-
-    // const img = await imgur.uploadFile('/home/ann/Documents/04_BAM/build-a-mate/public/image/test-face2.png', id)
-    // console.log('LINK:', img)
-    const album = 'qizwMyP'
+    const id = 'qizwMyP'
     const config = {
       headers: {
-        Authorization: 'Bearer ' + process.env.IMGUR_ACCESS_TOKEN
-        // Accept: 'application/json'
+        Authorization: 'Bearer ' + process.env.IMGUR_ACCESS_TOKEN,
+        Accept: 'application/json'
       }
     }
-    const data = req.body
-    // console.log(data)
-    const img = await axios.post(
-      `https://api.imgur.com/3/album/${album}/add`,
-      data,
-      config
-    )
-    console.log(img)
-    res.sendStatus(200)
+
+    console.log('REQ BODY:', req.body)
+    const data = {
+      album: id,
+      title: 'test',
+      name: 'xxx.png',
+      image: req.body
+    }
+    //const img = await axios.post(`https://api.imgur.com/3/album/${id}/add`, req.body, config)
+    const img = await axios.post(`https://api.imgur.com/3/image`, data, config)
+    //console.log(img.config)
+    res.send(img.data)
   } catch (err) {
     next(err)
   }
@@ -40,10 +36,48 @@ router.post('/uploadbwface', async (req, res, next) => {
 router.get('/bw', async (req, res, next) => {
   try {
     const id = 'qizwMyP'
-    const album = await axios.get(`https://api.imgur.com/3/album/${id}`)
+    const config = {
+      headers: {
+        Authorization: 'Bearer ' + process.env.IMGUR_ACCESS_TOKEN,
+        Accept: 'application/json'
+      }
+    }
+
+    // AXIOS
+    const images = await axios.get(
+      `https://api.imgur.com/3/album/${id}/images`,
+      config
+    )
+    console.log('IMGES:', images.data)
+    res.send(images.data)
+
+    // METHOD 1 : XML HTTP
+    // const XHR = new XMLHttpRequest()
+    // XHR.open('GET', `https://api.imgur.com/3/album/${id}/images`, true); // true for asynchronous
+    // XHR.setRequestHeader('Authorization', 'Bearer ' + process.env.IMGUR_ACCESS_TOKEN)
+    // XHR.setRequestHeader('Accept', 'application/json')
+    // await XHR.send();
+    // console.log(XHR)
+    // console.log('STATE:', XHR.readyState)
+    // console.log('RES TEXT:', XHR.responseText)
+
+    // if (req.readyState == 4 && req.status === 200) {
+    //   console.log(XHR)
+    //   console.log('STATE:', XHR.readyState)
+    //   console.log('RES TEXT:', XHR.responseText)
+    //   const json = JSON.parse(XHR.responseText)
+    //   console.log(json)
+    //   res.send(json)
+    // } else {
+    //   console.log('Error with Imgur Request.')
+    //   res.sendStatus(500)
+    // }
+
+    // LIB IMGUR
     // const album = await imgur.getAlbumInfo(id)
-    console.log(album)
-    res.send(album)
+    // console.log(album)
+
+    // res.send(album)
   } catch (err) {
     next(err)
   }
