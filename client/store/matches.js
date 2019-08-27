80067e75a1809fad27c9b5e8bfc49294fce23937
ANN LIN <smilemarg@gmail.com>
2019-08-27 16:34:10 -0400

    before merge

--- client/store/matches.js
+++ client/store/matches.js
@@ -8,43 +8,39 @@ const GOT_MATCHES = 'GOT_MATCHES'
 const gotMatches = matches => ({type: GOT_MATCHES, matches})
 
 // THUNK CREATORS
-export const getMatches = () =>
-  // gender,
-  // genderPreference,
-  // createdFaceDesc
-  async (dispatch, getState) => {
-    try {
-      const state = getState()
-      //onst id = state.user.id
-      const {data} = await axios.get(`/api/users/matches/${state.user.id}`)
-      const parsedCreatedFaceDesc = await new Float32Array(
-        Object.values(JSON.parse(state.user.createdFaceDesc))
-      )
-      let matches = await Promise.all(
-        data.map(async match => {
-          const parsedBwFaceDesc = await new Float32Array(
-            Object.values(JSON.parse(match.bwFaceDesc))
-          )
-          const euclideanDistance = await faceapi.euclideanDistance(
-            parsedCreatedFaceDesc,
-            parsedBwFaceDesc
-          )
-          return {...match, euclideanDistance}
-        })
-      )
-      // console.timeEnd('map')
-      // console.time('sort')
-      matches = matches
-        .sort((a, b) => a.euclideanDistance - b.euclideanDistance)
-        .slice(0, 5)
-      // console.timeEnd('sort')
-      // console.timeEnd('total')
-      console.log(matches)
-      dispatch(gotMatches(matches))
-    } catch (err) {
-      console.error(err)
-    }
+export const getMatches = () => async (dispatch, getState) => {
+  try {
+    const state = getState()
+    //onst id = state.user.id
+    const {data} = await axios.get(`/api/users/matches/${state.user.id}`)
+    const parsedCreatedFaceDesc = await new Float32Array(
+      Object.values(JSON.parse(state.user.createdFaceDesc))
+    )
+    let matches = await Promise.all(
+      data.map(async match => {
+        const parsedBwFaceDesc = await new Float32Array(
+          Object.values(JSON.parse(match.bwFaceDesc))
+        )
+        const euclideanDistance = await faceapi.euclideanDistance(
+          parsedCreatedFaceDesc,
+          parsedBwFaceDesc
+        )
+        return {...match, euclideanDistance}
+      })
+    )
+    // console.timeEnd('map')
+    // console.time('sort')
+    matches = matches
+      .sort((a, b) => a.euclideanDistance - b.euclideanDistance)
+      .slice(0, 5)
+    // console.timeEnd('sort')
+    // console.timeEnd('total')
+    console.log(matches)
+    dispatch(gotMatches(matches))
+  } catch (err) {
+    console.error(err)
   }
+}
 
 const initialState = []
 
