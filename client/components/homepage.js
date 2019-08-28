f35d9cdc7a1419910c2099645ccdc53a87647db9
YC <ymmat55@gmail.com>
2019-08-28 17:05:42 -0400

    add image on homepage

--- client/components/homepage.js
+++ client/components/homepage.js
@@ -3,29 +3,34 @@ import React from 'react'
 
 export default function homepage() {
   return (
-    <div id="carouselbody">
-      <Carousel interval={5000}>
-        <Carousel.Item>
-          <div className="imgBody">
-            <img className="carousel-Img" src="/image/Scarlett.png" />
-            <img className="carousel-Img" src="/image/Steven.png" />
-            <img className="carousel-Img" src="/image/DJ.png" />
-          </div>
-          <Carousel.Caption>
-            <h3>Build Your Perfect Match</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="imgBody">
-            <img className="carousel-Img" src="/image/BeyonceProfile.png" />
-            <img className="carousel-Img" src="/image/Lance.png" />
-            <img className="carousel-Img" src="/image/Adriana.png" />
-          </div>
-          <Carousel.Caption>
-            <h3>Build Your Perfect Match</h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+    <div id="container">
+      <div id="carouselbody">
+        <Carousel interval={5000}>
+          <Carousel.Item>
+            <div className="imgBody">
+              <img className="carousel-Img" src="/image/Scarlett.png" />
+              <img className="carousel-Img" src="/image/Steven.png" />
+              <img className="carousel-Img" src="/image/DJ.png" />
+            </div>
+            <Carousel.Caption>
+              <h3>Build Your Perfect Match</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="imgBody">
+              <img className="carousel-Img" src="/image/BeyonceProfile.png" />
+              <img className="carousel-Img" src="/image/Lance.png" />
+              <img className="carousel-Img" src="/image/Adriana.png" />
+            </div>
+            <Carousel.Caption>
+              <h3>Build Your Perfect Match</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+      <div id="illustration">
+        <img src="/image/homepageUse2.png" />
+      </div>
     </div>
   )
 }
